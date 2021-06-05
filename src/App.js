5928a28d8cc0b831d39cd2ffc95510f2a52d4c2c
Tosin Akerele <akerele.james1699@gmail.com>
2021-06-05 21:51:36 +0100

    more commits

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './Components/Card-list/card-list.component';
+import { SearchBox } from './Components/Searchbox/Search-box.component';
+
+class App extends Component {
+
+  constructor (){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount(){
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url) 
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state; //This is the same as writing: 
+    //const monsters = this.state.monsters;
+    //const searchField = this.state.searchField;
+
+    //PERFORMING THE FILTERING OPERATION NOW.
+    const filteredMonsters = monsters.filter( monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return ( 
+      <div className="App">
+      <h1>Monster Rolodex</h1>
+        <SearchBox 
+          placeholder="Search Monsters" 
+          handleChange = { this.handleChange }  
+        /> 
+        <CardList monsters={ filteredMonsters }/>
+      </div>
+    )
+  }
+}
+
+export default App; 
---
+++ src/Components/Card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) => (
+    <div className='card-container'>
+        <img alt="monsters" src={`https://robohash.org/${props.monster.id}?set=set2`}/>
+        <h2> { props.monster.name } </h2>
+        <h4> { props.monster.username }</h4>        
+        <h4> { props.monster.email }</h4>        
+    </div>
+)
